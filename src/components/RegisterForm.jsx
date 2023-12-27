583bd52fa2a8d1601a59cbf313b9571c0fdbcbb6
Kirstin <christine.gulmatico@g.msuiit.edu.ph>
2023-12-27 17:16:50 +0800

    added input validation for registration.

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -4,9 +4,46 @@ import { Link } from 'react-router-dom';
 function RegisterForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const validatePassword = (password) => {
+    // Password should be at least 8 characters with at least 1 uppercase, 1 numeric, and 1 symbol
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$/;
+    const isValid = passwordRegex.test(password);
+
+    if (!isValid) {
+      setError('Password must be at least 8 characters with at least 1 uppercase, 1 numeric, and 1 symbol.');
+    } else {
+      setError('');
+    }
+
+    return isValid;
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    validatePassword(e.target.value);
+  };
+
+  const passwordValidationStyle = {
+    color: error ? 'red' : 'green',
+    fontSize: '12px',
+    fontWeight: '700',
+    transition: 'color 0.3s', // Add a smooth transition effect
+  };
 
   const handleRegister = async (e) => {
     e.preventDefault();
+
+    if (!validateEmail(email) || !validatePassword(password)) {
+      setError('Invalid email or password. Please check your input.');
+      return;
+    }
 
     try {
         const response = await fetch('http://localhost:8080/user/register', {
@@ -28,7 +65,7 @@ function RegisterForm() {
         console.error('Error during registration:', error);
       }
     };
-    // Add your login logic here
+   
   return (
     <form onSubmit={handleRegister} className="template-form">
     <h2>Sign up an account.</h2>
@@ -44,9 +81,14 @@ function RegisterForm() {
         type="password"
         id="password"
         value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        onChange={handlePasswordChange}
         placeholder="Password"
       />
+      <div className="data-validation">
+        <label style={passwordValidationStyle}>
+          Password must be at least 8 characters long with one uppercase character, number, and symbol.
+        </label>
+      </div>
       <div>
         <h3>By clicking "Sign up," you agree to our Terms of Use and our Privacy Policy.</h3>
       </div>
